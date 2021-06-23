b9ef4bff99b81a953ca67bc561ad0f3867e09706
johnrowan <copypasteearth@gmail.com>
2021-06-23 13:27:07 -0400

    added object detection model

--- app/src/main/java/jacs/apps/mlbucket/ui/image_classification/BirdClassificationViewModel.kt
+++ app/src/main/java/jacs/apps/mlbucket/ui/image_classification/BirdClassificationViewModel.kt
@@ -11,6 +11,7 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import jacs.apps.mlbucket.ml.LiteModelAiyVisionClassifierBirdsV13
 import jacs.apps.mlbucket.ml.LiteModelAiyVisionClassifierFoodV11
+import jacs.apps.mlbucket.ml.LiteModelObjectDetectionMobileObjectLabelerV11
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.label.Category
 
@@ -23,6 +24,7 @@ class BirdClassificationViewModel(application: Application) : AndroidViewModel(a
         when(position){
             0 -> birds(context)
             1 -> food(context)
+            2 -> objects(context)
         }
 
 
@@ -61,6 +63,36 @@ class BirdClassificationViewModel(application: Application) : AndroidViewModel(a
     fun food(context: Context){
         var bitmap : Bitmap
         val model = LiteModelAiyVisionClassifierFoodV11.newInstance(context)
+        if(Build.VERSION.SDK_INT < 28) {
+            bitmap = MediaStore.Images.Media.getBitmap(
+                context.contentResolver,
+                imageUri
+            )
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+        } else {
+            val source = ImageDecoder.createSource(context.contentResolver, imageUri!!)
+            bitmap = ImageDecoder.decodeBitmap(source)
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        }
+        val image = TensorImage.fromBitmap(bitmap)
+
+// Runs model inference and gets result.
+        val outputs = model.process(image)
+        var prob = outputs.probabilityAsCategoryList.apply {
+            sortByDescending { it.score } // Sort with highest confidence first
+        }.take(10) // take the top results
+        probabilities = prob as ArrayList<Category>;
+
+
+
+// Releases model resources if no longer used.
+        model.close()
+        Log.d("bitmap", probabilities[0].score.toString())
+    }
+    fun objects(context: Context){
+        var bitmap : Bitmap
+        val model = LiteModelObjectDetectionMobileObjectLabelerV11.newInstance(context)
         if(Build.VERSION.SDK_INT < 28) {
             bitmap = MediaStore.Images.Media.getBitmap(
                 context.contentResolver,
