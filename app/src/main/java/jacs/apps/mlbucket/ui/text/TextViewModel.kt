61352fd31c9fef5ae82b5462dd9155efefb4c9b9
johnrowan <copypasteearth@gmail.com>
2021-06-29 09:19:28 -0400

    testing library kotlinwikipediasearch for use with BERT mobile later

--- app/src/main/java/jacs/apps/mlbucket/ui/text/TextViewModel.kt
+++ app/src/main/java/jacs/apps/mlbucket/ui/text/TextViewModel.kt
@@ -3,11 +3,24 @@ package jacs.apps.mlbucket.ui.text
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import jacs.apps.kotlinwiki.KotlinWiki
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class TextViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        viewModelScope.launch(Dispatchers.IO){
+            val wikiSearch = KotlinWiki()
+            val response = wikiSearch.searchResponse("hello world")
+            val page = wikiSearch.getPage(response.query?.pages!![0].title!!)
+            viewModelScope.launch(Dispatchers.Main){
+                value = wikiSearch.getSimplyParsedWikiText()
+            }
+
+        }
+        //value = "This is gallery Fragment"
     }
     val text: LiveData<String> = _text
 }
